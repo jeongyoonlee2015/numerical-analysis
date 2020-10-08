0335319a51fced085ec17792ecd7f06bbf649a81
jeongyoonlee2015 <happyjoy2005@gmail.com>
2020-10-08 14:20:37 +0900

    add ch2

---
+++ chapter_2/console-practice.py
@@ -0,0 +1,20 @@
+# for문
+for i in range (0,3):
+     print(i)
+
+for i in range(3):
+    print(i)
+
+# for i in range (0,3):
+#   ...:     print(i)
+#   ...:
+# 0
+# 1
+# 2
+# for i in range(3):
+#   ...:     print(i)
+#   ...:
+# 0
+# 1
+# 2
+
